098a7608a3bac2893ac0e327cb529ad40731991d
Dima Koltovich <dima.koltovych@jet.dev>
2020-09-04 15:30:37 +0300

    Fix README file and some other minor fixes

--- standalone/run.sh
+++ standalone/run.sh
@@ -14,6 +14,6 @@ if [ -z $2 ]; then
   exit 1;
 fi
 
-echo "[host]\n$1 ansible_connection=ssh ansible_user=root ansible_python_interpreter=/usr/bin/python3" > standalone.inventory
+echo "[all]\n$1 ansible_connection=ssh ansible_user=root" > standalone.inventory
 
 ansible-playbook -v -i standalone.inventory -e rsyslog_client_hostname=$1 -e rsyslog_server_hostname=$2 run.playbook.yml
